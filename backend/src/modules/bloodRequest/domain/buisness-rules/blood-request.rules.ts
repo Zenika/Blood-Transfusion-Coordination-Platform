27041f49351532b27b02b54244d4d927812d4336
achraf <achrafhafid565@gmail.com>
2026-07-21 10:33:00 +0100

    feat: create  blood request updating  buisness rule

--- backend/src/modules/bloodRequest/domain/buisness-rules/blood-request.rules.ts
+++ backend/src/modules/bloodRequest/domain/buisness-rules/blood-request.rules.ts
@@ -4,6 +4,7 @@ import {
   Injectable,
 } from '@nestjs/common';
 import { BloodRequestStatus } from 'src/shared/enums/blood-request-status.enum';
+import { BloodRequestEntity } from '../entities/blood-request.entity';
 
 @Injectable()
 export class BloodRequestBuisnessRules {
@@ -44,5 +45,10 @@ export class BloodRequestBuisnessRules {
         `Invalid status transition from ${currentStatus} to ${newStatus}`,
       );
     }
+  }
+
+  ensureRequestCanBeUpdated(bloodRequest: BloodRequestEntity) {
+    if (bloodRequest.status !== BloodRequestStatus.PENDING)
+      throw new BadRequestException("Blood request can't be modified");
   }
 }
