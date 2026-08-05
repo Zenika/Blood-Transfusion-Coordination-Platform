cfd5d46287855550cf85c08fe778b1a0025d8124
achraf <achrafhafid565@gmail.com>
2026-08-05 10:06:17 +0100

    fix: current user decorator type

--- backend/src/shared/decorators/current-user.decorator.ts
+++ backend/src/shared/decorators/current-user.decorator.ts
@@ -1,9 +1,12 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { Request } from 'express';
 import { AuthenticatedUser } from '../types/authenticated-user.type';
 
+type RequestWithUser = Request & { user?: AuthenticatedUser };
+
 export const CurrentUser = createParamDecorator(
-  (data: unknown, context: ExecutionContext): AuthenticatedUser => {
-    const request = context.switchToHttp().getRequest();
+  (data: unknown, context: ExecutionContext): AuthenticatedUser | undefined => {
+    const request = context.switchToHttp().getRequest<RequestWithUser>();
     return request.user;
   },
 );
