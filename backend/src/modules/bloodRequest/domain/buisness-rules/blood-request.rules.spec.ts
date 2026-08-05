a44c8194d00f360173dd064941e6fc7f233769f7
achraf <achrafhafid565@gmail.com>
2026-08-05 15:11:21 +0100

    feat:add ensure status transtion valid test

--- backend/src/modules/bloodRequest/domain/buisness-rules/blood-request.rules.spec.ts
+++ backend/src/modules/bloodRequest/domain/buisness-rules/blood-request.rules.spec.ts
@@ -1,5 +1,6 @@
 /// <reference types="jest" />
 
+import { BloodRequestStatus } from 'src/shared/enums/blood-request-status.enum';
 import { BloodRequestBuisnessRules } from './blood-request.rules';
 
 describe('Ensure quantity positive', () => {
@@ -10,6 +11,21 @@ describe('Ensure quantity positive', () => {
   it('should ensure quantity positive', () => {
     expect(() =>
       bloodRequestBuisnessRules.ensureQuantityIsPositive(-1),
+    ).toThrow();
+  });
+});
+
+describe(' Ensure transition status valid', () => {
+  let bloodRequestBuisnessRules: BloodRequestBuisnessRules;
+  beforeEach(() => {
+    bloodRequestBuisnessRules = new BloodRequestBuisnessRules();
+  });
+  it('should ensure status transition valid', () => {
+    expect(() =>
+      bloodRequestBuisnessRules.ensureStatusTransitionIsValid(
+        BloodRequestStatus.ACCEPTED,
+        BloodRequestStatus.MATCHING,
+      ),
     ).toThrow();
   });
 });
